facb2e5a35f53f0149a5bc56380fbe44ee9b11cd
index7777 <paromi7777@gmail.com>
2026-08-19 19:17:23 +0800

    chore: remove global sprite debug compatibility hook

--- src/main.ts
+++ src/main.ts
@@ -6,13 +6,6 @@ import{JourneyScene}from'./presentation/scenes/JourneyScene';
 import './presentation/runtime/P11RuntimeWiring';
 import './presentation/runtime/ProductionRuntime';
 
-const spritePrototype=Phaser.GameObjects.Sprite.prototype as unknown as{setTint:(...colors:number[])=>Phaser.GameObjects.Sprite};
-const originalSpriteSetTint=spritePrototype.setTint;
-spritePrototype.setTint=function(...colors:number[]){
-  if(colors[0]===0xff5060)return this as unknown as Phaser.GameObjects.Sprite;
-  return originalSpriteSetTint.apply(this,colors)
-};
-
 for(const eventName of ['contextmenu','dragstart','selectstart'] as const){
   document.addEventListener(eventName,(event)=>event.preventDefault(),{capture:true});
 }
